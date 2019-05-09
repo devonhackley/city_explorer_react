0a988abfa29eed80278a5685617d56a14fa85075
Devon Hackley <devonhackley@gmail.com>
2019-05-08 22:17:58 -0700

    finished search-form

--- src/app.js
+++ src/app.js
@@ -1,35 +1,8 @@
 import React from 'react';
+import Header from './header';
+import SearchForm from './search-form';
 
-/** Header Component **/
-class Header extends React.Component {
-  render(){
-    return (
-      <React.Fragment>
-        <h1>City Explorer</h1>
-        <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
-      </React.Fragment>
-    )
-  }
-}
 
-/** Search Component **/
-class Search  extends React.Component {
-  constructor(props){
-    super(props);
-    this.state ={}
-  }
-  render(){
-    return (
-      <React.Fragment>
-      <form id="search-form" class="hide">
-        <label for="search">Search for a location</label>
-        <input type="text" name="search" id="input-search" placeholder="Enter a location here" />
-        <button type="submit">Explore!</button>
-      </form>
-      </React.Fragment>
-    )
-  }
-}
 
 /** Map Component **/
 class Map extends React.Component {
@@ -42,7 +15,7 @@ class Map extends React.Component {
       <React.Fragment>
         {/* <img id="map" class="hide" src="https://www.google.com/maps/place/Seattle,+WA/@47.6129432,-122.4821475,11z/data=!3m1!4b1!4m5!3m4!1s0x5490102c93e83355:0x102565466944d59a!8m2!3d47.6062095!4d-122.3320708" alt="Map of search query" /> */}
 
-        <img src="https://placehold.it/960x450/000" />
+        <img src="https://placehold.it/960x450/000" alt="placeh"/>
       </React.Fragment>
     )
   }
@@ -72,18 +45,51 @@ class Result extends React.Component {
 class Main  extends React.Component {
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+      location: {},
+      apiURL: '',
+      geoKey: ''
+    }
+  }
+  //handler function that makes requests
+  // makeRequests = () => {
+
+  // }
+  handleFormSubmit = (val) => {
+    const { apiURL, geoKey, location } = this.state;
+    !apiURL ? 
+      this.setState({ apiURL: val}) :
+        !geoKey ?
+          this.setState({ geoKey: val}) :
+            console.log('stuff');
   }
   render(){
+    const { apiURL, geoKey, location } = this.state;
+    let form = apiURL && geoKey ? (
+      <SearchForm 
+        handler={this.handleFormSubmit} 
+        appUrl={this.state.apiURL}
+        label="Search for a location"
+      />
+    ) : (
+        <React.Fragment>
+          <SearchForm 
+            handler={this.handleFormSubmit} 
+            appUrl={this.state.apiURL}
+            label="Enter the URL to your deployed back end, making sure to remove the trailing forward slash"
+          />
+          <SearchForm 
+            handler={this.handleFormSubmit} 
+            appUrl={this.state.apiURL}
+            label="Enter your Geocoding API Key:"
+          />
+        </React.Fragment>
+    )
     return (
       <React.Fragment>
-        <Search />
-        <Map />
-        <Result />
-        <Result />
-        <Result />
-        <Result />
-        <Result />
+        {form}
+        <Map location={location} geoKey/>
+        {/* <SearchResults location={this.state.location}/> */}
       </React.Fragment>
     )
   }
